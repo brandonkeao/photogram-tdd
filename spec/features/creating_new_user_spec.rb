33106dbb5e45090740e9395e2ec72ab85acf74d3
brandonkeao <brandon.keao@gmail.com>
2017-05-17 06:28:59 -0600

    Created new specs for testing user name min and max length

--- spec/features/creating_new_user_spec.rb
+++ spec/features/creating_new_user_spec.rb
@@ -15,4 +15,26 @@ feature 'Create a new user' do
 		click_button 'Sign up'
 		expect(page).to have_content('Welcome! You have signed up successfully.')
 	end
+
+	scenario 'requires a user name to successfully create an account' do
+		fill_in 'Email', with: 'sxyrailsdev@myspace.com'
+		fill_in 'Password', with: 'supersecret', match: :first
+		fill_in 'Password confirmation', with: 'supersecret'
+
+		click_button 'Sign up'
+		expect(page).to have_content("can't be blank")
+	end
+
+	scenario 'requires a user name to be more than 4 characters' do
+		fill_in 'User name', with: 'h'
+		fill_in 'Email', with: 'sxyrailsdev@myspace.com'
+		fill_in 'Password', with: 'supersecret', match: :first
+		fill_in 'Password confirmation', with: 'supersecret'
+
+		click_button 'Sign up'
+		expect(page).to have_content("minimum is 4 characters")
+
+
+	end
+
 end
